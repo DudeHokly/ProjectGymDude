fe05a448473c254e3b75ef375044bcc7eff7bc08
preytech <pimonov.makar@mail.ru>
2024-03-27 16:32:14 +0300

    kakoitoCommit

--- src/components/RecomendBlockGym/kartochkiAnimated.js
+++ src/components/RecomendBlockGym/kartochkiAnimated.js
@@ -1,49 +1,26 @@
-export default function KartochkaAnimated() {
+const list = document.querySelector("list");
+
+export default function KartochkaAnimated({ cards }) {
+  const cardsJsx = cards.map((elem, i) => {
+    return (
+      <Kartochka
+        icon={elem.icon}
+        title={elem.title}
+        text={elem.text}
+        href={elem.link}
+      />
+    );
+  });
+  return <div className="kartochkiAnimated">{cardsJsx}</div>;
+}
+
+function Kartochka({ icon, title, text, href }) {
   return (
-    <div className="kartochkiAnimated">
-      <div className="Kartochka">
-        <div className="icon">
-          <i className="icon-accessibility"></i>
-        </div>
-        <h2 className="ZagolovKartoch">FLEX MUSCLE</h2>
-        <p className="textCard">
-          Ты можешь стать сильнее только когда ты сильный и можешь стать слабее
-          когда ты слабый
-        </p>
-        <a href="#">Узнать больше</a>
-      </div>
-      <div className="Kartochka">
-        <div className="icon">
-          <i className="icon-blind"></i>
-        </div>
-        <h2 className="ZagolovKartoch">BIG WEIGHT LIFTING</h2>
-        <p className="textCard">
-          Поднимай большие веса и становись большим, потому что ты можешь ты
-          можешь все
-        </p>
-        <a href="#">Узнать больше</a>
-      </div>
-      <div className="Kartochka">
-        <div className="icon">
-          <i className="icon-person"></i>
-        </div>
-        <h2 className="ZagolovKartoch">POWERFUL VITAMINS</h2>
-        <p className="textCard">
-          Ешь много витаминов и не забывай покупать наши витамины, почему потому
-          что ты можешь ты можешь все
-        </p>
-        <a href="#">Узнать больше</a>
-      </div>
-      <div className="Kartochka">
-        <div className="icon">
-          <i className="icon-blind-1"></i>
-        </div>
-        <h2 className="ZagolovKartoch">JUST PUMPING</h2>
-        <p className="textCard">
-          Просто качай железо, чтобы оно не качало тебя
-        </p>
-        <a href="#">Узнать больше</a>
-      </div>
+    <div className="Kartochka">
+      <div className="icon">{icon}</div>
+      <h2 className="ZagolovKartoch">{title}</h2>
+      <p className="textCard">{text}</p>
+      <a href={href}>Узнать больше</a>
     </div>
   );
 }
--- src/components/RecomendBlockGym/recomendBlock.js
+++ src/components/RecomendBlockGym/recomendBlock.js
@@ -2,6 +2,33 @@ import KartochkaAnimated from "./kartochkiAnimated";
 import "./recomendBlock.css";
 import "../../icons/css/fontello.css";
 
+const cards = [
+  {
+    icon: <i className="icon-accessibility"></i>,
+    title: "FLEX MUSCLE",
+    text: "Ты можешь стать сильнее только когда ты сильный и можешь стать слабее когда ты слабый",
+    link: "#",
+  },
+  {
+    icon: <i className="icon-blind"></i>,
+    title: "BIG WEIGHT LIFTING",
+    text: "Поднимай большие веса и становись большим, потому что ты можешь ты можешь все",
+    link: "#",
+  },
+  {
+    icon: <i className="icon-person"></i>,
+    title: "POWERFUL VITAMINS",
+    text: "Ешь много витаминов и не забывай покупать наши витамины, почему потому что ты можешь ты можешь все",
+    link: "#",
+  },
+  {
+    icon: <i className="icon-blind-1"></i>,
+    title: "JUST PUMPING",
+    text: "Просто качай железо, чтобы оно не качало тебя",
+    link: "#",
+  },
+];
+
 export default function RecomendBlock() {
   return (
     <section className="recomendBlockGym">
@@ -10,7 +37,7 @@ export default function RecomendBlock() {
           <h2 className="textRecomend">Почему стоит выбрать нас</h2>
           <h1 className="textRecomend2">Big PUMP</h1>
         </div>
-        <KartochkaAnimated />
+        <KartochkaAnimated cards={cards} />
       </div>
     </section>
   );
