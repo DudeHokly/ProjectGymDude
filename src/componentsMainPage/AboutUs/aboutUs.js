f9c7e7db788c09bdb4ea8f02b62f186d3b399661
DudeHokly <ursus12334@gmail.com>
2024-04-19 19:52:43 +0300

    update037.7

--- src/componentsMainPage/AboutUs/aboutUs.js
+++ src/componentsMainPage/AboutUs/aboutUs.js
@@ -1,43 +1,45 @@
 import "./aboutUs.css";
 
 // const counterNumbers =
-
+// py-[120px] pr-[90px] pl-[550px]
 export default function AboutUS() {
   return (
-    <section className="aboutUS">
-      <div className="contentUs _conteiner">
-        <div className="conteinerUs">
-          <h2 className="textAbout">О нас</h2>
-          <h1 className="textWelcome">ДОБРО ПОЖАЛОВАТЬ В XTREME FITNESS</h1>
-          <p className="textJust">
-            Это не просто клуб для любителей спорта и людей, которые ведут
-            здоровый образ жизни. Наша площадка – это настоящий стиль жизни,
-            который в первую очередь гарантирует не только профессиональный
-            подход к занятию спортом, а отвечает на главный вопрос: кто герой и
-            героиня нашего времени.
-          </p>
-          <div className="allResultAbout">
-            <div className="resultsAbout">
-              <h3>600k+</h3>
-              <p>РАБОЧИХ ЧАСОВ</p>
+    <section className="aboutUS Large:w-[100%] ExtraLarge:w-[100%]">
+      <div className="contentUs Large:w-[430px] _conteiner">
+        <div className="conteinerUs flex justify-end">
+          <div className="#">
+            <h2 className="textAbout">О нас</h2>
+            <h1 className="textWelcome">ДОБРО ПОЖАЛОВАТЬ В XTREME FITNESS</h1>
+            <p className="textJust">
+              Это не просто клуб для любителей спорта и людей, которые ведут
+              здоровый образ жизни. Наша площадка – это настоящий стиль жизни,
+              который в первую очередь гарантирует не только профессиональный
+              подход к занятию спортом, а отвечает на главный вопрос: кто герой
+              и героиня нашего времени.
+            </p>
+            <div className="allResultAbout">
+              <div className="resultsAbout">
+                <h3>600k+</h3>
+                <p>РАБОЧИХ ЧАСОВ</p>
+              </div>
+              <div className="resultsAbout">
+                <h3>790+</h3>
+                <p>ПРОГРАММ ТРЕНИРОВОК</p>
+              </div>
+              <div className="resultsAbout">
+                <h3>2560+</h3>
+                <p>СЧАСТЛИВЫХ КЛИЕНТОВ</p>
+              </div>
+              <div className="resultsAbout">
+                <h3>840+</h3>
+                <p>ИДЕАЛЬНЫЙ РЕЗУЛЬТАТ</p>
+              </div>
             </div>
-            <div className="resultsAbout">
-              <h3>790+</h3>
-              <p>ПРОГРАММ ТРЕНИРОВОК</p>
+            <div className="buttonJoin">
+              <a href="#" className="buttonUs">
+                Присоединиться
+              </a>
             </div>
-            <div className="resultsAbout">
-              <h3>2560+</h3>
-              <p>СЧАСТЛИВЫХ КЛИЕНТОВ</p>
-            </div>
-            <div className="resultsAbout">
-              <h3>840+</h3>
-              <p>ИДЕАЛЬНЫЙ РЕЗУЛЬТАТ</p>
-            </div>
-          </div>
-          <div className="buttonJoin">
-            <a href="#" className="buttonUs">
-              Присоединиться
-            </a>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      Medium: "576px",
+      // => @media (min-width:576px)
+      Large: "992px",
+      // => @media (min-width:992px)
+      ExtraLarge: "1110px",
+      // => @media (min-width:992px)
+    },
     extend: {
       fontFamily: {
         FontChange: ["Carlito", "serif"],
