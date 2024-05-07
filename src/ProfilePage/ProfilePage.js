4f5f08b887773be7d7d68167e1e6201ca72acfc9
DudeHokly <ursus12334@gmail.com>
2024-05-07 23:03:03 +0300

    little extra commit

--- src/ProfilePage/ProfilePage.js
+++ src/ProfilePage/ProfilePage.js
@@ -3,16 +3,78 @@ import "./ProfilePage.css";
 export default function Profile() {
   return (
     <section className="yourProfile">
-      <div className="profileInfo">
-        <h2>Welcome, [Username]</h2>
-        <p>Email: [User's Email]</p>
-        <p>Phone: [User's Phone Number]</p>
-        <p>Address: [User's Address]</p>
-      </div>
-      <div className="profileActions">
-        <button>Edit Profile</button>
-        <button>Change Password</button>
-        <button>Logout</button>
+      <div className="container">
+        <div className="main-body">
+          <div className="row gutters-sm">
+            <div className="col-md-4 mb-3">
+              <div className="card">
+                <div className="card-body">
+                  <div className="">
+                    <img
+                      src="https://bootdey.com/img/Content/avatar/avatar7.png"
+                      alt="Admin"
+                      className="rounded-circle"
+                      width="150"
+                    />
+                    <div className="">
+                      <h4>John Doe</h4>
+                      <p className="">Full Stack Developer</p>
+                      <p className="">Bay Area, San Francisco, CA</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <div className="card">
+                <div className="card-body">
+                  <div className="row">
+                    <div className="">
+                      <h6 className="">Full Name</h6>
+                    </div>
+                    <div className="c">Kenneth Valdez</div>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="">
+                      <h6 className="">Email</h6>
+                    </div>
+                    <div className="">fip@jukmuh.al</div>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="">
+                      <h6 className="">Phone</h6>
+                    </div>
+                    <div className="">(239) 816-9029</div>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="">
+                      <h6 className="">Mobile</h6>
+                    </div>
+                    <div className="">(320) 380-4539</div>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <h6 className="mb-0">Address</h6>
+                    </div>
+                    <div className="text-secondary">
+                      Bay Area, San Francisco, CA
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="">
+                    <div className="">
+                      <a className="">Edit</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
